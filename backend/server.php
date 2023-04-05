7e02e30a650c94512d5ac80792dae212c8d8f952
JasonLukassen <113337677+JasonLukassen@users.noreply.github.com>
2023-04-05 14:56:00 +0200

    update
    attempt at connecting to database

---
+++ backend/server.php
@@ -0,0 +1,31 @@
+<?php
+// MySQL server configuration
+$servername = "paypaloozaserver";
+$username = "JasonWong";
+$password = "ThisIsThePaypaloozaServer";
+$dbname = "paypalooza";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Run a sample query
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+
+?>
