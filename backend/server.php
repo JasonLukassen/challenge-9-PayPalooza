67eff639c24e14854f30375ee97f5672f807849a
JasonLukassen <113337677+JasonLukassen@users.noreply.github.com>
2023-04-06 14:59:45 +0200

    update
    login page backend started

--- backend/server.php
+++ backend/server.php
@@ -13,19 +13,37 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Run a sample query
-$sql = "SELECT * FROM users";
-$result = $conn->query($sql);
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+    $username = mysqli_real_escape_string($conn, $username);
+    $password = mysqli_real_escape_string($conn, $password);
+    
+    $loginCheck = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+
+    $result = mysqli_query($conn, $loginCheck);
+    
+    if (mysqli_num_rows($result) === 1){
+        session_start();
+        $_SESSION['username'] = $username;
+    } else{
+        $error_message = "Invalid username or password";
     }
-} else {
-    echo "0 results";
+
+    // Run a sample query
+    $sql = "SELECT * FROM users";
+    $result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " - username: " . $row["username"]. " - password: " . $row["password"]. "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
 }
 
-$conn->close();
 
 ?>
